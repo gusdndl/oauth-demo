5546d7132c58626b93de55479e4412f2452f89ae
gusdndl <gusdndl03@gmail.com>
2026-02-02 21:33:32 +0900

    Initial commit

---
+++ Backend/Oauth2/src/main/java/com/example/Oauth2/oauth2/user/OAuth2UserInfoFactory.java
@@ -0,0 +1,22 @@
+package com.example.Oauth2.oauth2.user;
+
+import com.example.Oauth2.oauth2.exception.OAuth2AuthenticationProcessingException;
+
+import java.util.Map;
+
+public class OAuth2UserInfoFactory {
+
+    public static OAuth2UserInfo getOAuth2UserInfo(String registrationId,
+                                                   String accessToken,
+                                                   Map<String, Object> attributes) {
+        if (OAuth2Provider.GOOGLE.getRegistrationId().equals(registrationId)) {
+            return new GoogleOAuth2UserInfo(accessToken, attributes);
+        } else if (OAuth2Provider.NAVER.getRegistrationId().equals(registrationId)) {
+            return new NaverOAuth2UserInfo(accessToken, attributes);
+        } else if (OAuth2Provider.KAKAO.getRegistrationId().equals(registrationId)) {
+            return new KaKaoOAuth2UserInfo(accessToken, attributes);
+        } else {
+            throw new OAuth2AuthenticationProcessingException("Login with " + registrationId + " is not supported");
+        }
+    }
+}
---
+++ Backend/Oauth2/src/main/java/com/example/Oauth2/oauth2/user/OAuth2UserUnlink.java
@@ -0,0 +1,5 @@
+package com.example.Oauth2.oauth2.user;
+
+public interface OAuth2UserUnlink {
+    void unlink(String accessToken);
+}
